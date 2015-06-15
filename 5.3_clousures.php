518958c17ca13db220f55575c0e20cf2664c1531
Emerson Rocha Luiz <emerson@alligo.com.br>
2015-06-15 01:35:03 -0300

    melhorado 5.3_clousures e 5.4_traits

--- 5.3_clousures.php
+++ 5.3_clousures.php
@@ -1,4 +1,38 @@
 <?php
 
 // http://php.net/manual/en/functions.anonymous.php
-// @todo
+
+
+echo preg_replace_callback('~-([a-z])~', function ($match) {
+    return strtoupper($match[1]);
+}, 'hello-world');
+// outputs helloWorld
+
+echo PHP_EOL . '-----------------------------------' . PHP_EOL;
+// por orls
+
+$result = 0;
+
+$one = function() {
+    var_dump($result);
+};
+
+$two = function() use ($result) {
+    var_dump($result);
+};
+
+$three = function() use (&$result) {
+    var_dump($result);
+};
+
+$result++;
+
+
+$one();
+$two();
+$three();
+
+//PHP Notice:  Undefined variable: result in /alligo/code/fititnt/php-playground/5.3_clousures.php on line 17
+//NULL
+//int(0)
+//int(1)
--- 5.4_traits.php
+++ 5.4_traits.php
@@ -1,3 +1,69 @@
 <?php
 
-// @todo
+//http://php.net/manual/pt_BR/language.oop5.traits.php
+class Base
+{
+
+    public function sayHello()
+    {
+        echo 'Hello ';
+    }
+}
+
+trait SayWorld
+{
+
+    public function sayHello()
+    {
+        parent::sayHello();
+        echo 'World!';
+    }
+}
+
+class MyHelloWorld extends Base
+{
+    use SayWorld;
+}
+
+$o = new MyHelloWorld();
+$o->sayHello();
+
+// Hello World!
+
+echo PHP_EOL . '-----------------------------------' . PHP_EOL;
+
+trait Hello
+{
+
+    public function sayHello()
+    {
+        echo 'Hello ';
+    }
+}
+
+trait World
+{
+
+    public function sayWorld()
+    {
+        echo 'World';
+    }
+}
+
+class MyHelloWorld2
+{
+    use Hello,
+        World;
+
+    public function sayExclamationMark()
+    {
+        echo '!';
+    }
+}
+
+$o = new MyHelloWorld2();
+$o->sayHello();
+$o->sayWorld();
+$o->sayExclamationMark();
+
+// Hello World!
