b768dfca9d0a016276dd07008702fd77c3d05652
Emerson Rocha Luiz <emerson@alligo.com.br>
2015-06-21 21:42:46 -0300

    redis: Adicionado PoC com Redis

---
+++ redis/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'vendor/autoload.php';
+
+Predis\Autoloader::register();
+
+$client = new Predis\Client();
+$client->set('foo', 'bar');
+$value = $client->get('foo');
+
+echo $value;
